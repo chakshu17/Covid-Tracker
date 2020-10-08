93fdac7961f40bc20cff6cb9ce16fd9d8aab14a1
Chakshu kishnani <chakshu05kishnani@gmail.com>
2020-10-08 12:24:21 +0530

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MaterialModuleModule } from '../app/modules/material-module/material-module.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    MaterialModuleModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/modules/material-module/material-module.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+import {MatCardModule} from '@angular/material/card';
+
+
+@NgModule({
+  declarations: [],
+  exports:[
+    MatCardModule,
+  ],
+  imports: [
+    CommonModule,
+    MatCardModule
+  ]
+})
+export class MaterialModuleModule { }
