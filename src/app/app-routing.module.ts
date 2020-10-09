2d752ab47ddb6429810edfc0625d741da06caf8a
Chakshu kishnani <chakshu05kishnani@gmail.com>
2020-10-09 18:52:09 +0530

    Update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
-
+import { StateWiseComponent } from './pages/state-wise/state-wise.component';
 
 const routes: Routes = [
-  {path:'home',component:HomeComponent},
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'state-wise', component: StateWiseComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
