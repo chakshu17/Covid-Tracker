49dc8959af8fd6d50db33bc0cfbc4ea6842f8afb
Chakshu kishnani <chakshu05kishnani@gmail.com>
2020-10-09 23:14:08 +0530

    update

--- src/app/pages/state-wise/state-wise.component.ts
+++ src/app/pages/state-wise/state-wise.component.ts
@@ -1,15 +1,39 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-state-wise',
   templateUrl: './state-wise.component.html',
-  styleUrls: ['./state-wise.component.scss']
+  styleUrls: ['./state-wise.component.scss'],
 })
 export class StateWiseComponent implements OnInit {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
-
+  apiData:any[];
+  stateName:string[] =[];
+  stateData:any=[]
   ngOnInit(): void {
+    this.http
+      .get('https://api.covidindiatracker.com/state_data.json')
+      .subscribe((re) => {
+          console.log(re);
+        this.apiData===re;
+
+        // this.apiData.map(data=>{
+        //   // console.log(data);
+        //   this.stateName.push(data.state);
+        //   this.stateData=data;
+        // })
+
+      });
+      // console.log(this.stateName);
+        console.log(this.apiData);
+
+  }
+
+  updateValues(country:string){
+      console.log(country);
+
   }
 
 }
