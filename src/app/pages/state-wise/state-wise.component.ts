2cb1cb86df87d5afcf931a889313398a89fbd7ee
Chakshu kishnani <chakshu05kishnani@gmail.com>
2020-10-20 10:53:36 +0530

    update

--- src/app/pages/state-wise/state-wise.component.ts
+++ src/app/pages/state-wise/state-wise.component.ts
@@ -9,29 +9,39 @@ import { Component, OnInit } from '@angular/core';
 export class StateWiseComponent implements OnInit {
   constructor(private http: HttpClient) {}
 
-  apiData: any[] = [];
+  apiData: any;
   stateName: string[] = [];
-  toatlCases: number;
+  toatlCases: number[]=[];
   stateData: any = [];
   ngOnInit(): void {
     this.http
       .get('https://api.covidindiatracker.com/state_data.json')
       .subscribe((re) => {
-        this.apiData.push(re);
+        this.apiData = re;
+        console.log('Inside',this.apiData);
+        this.apiData.map((data) => {
+            //  console.log(data);
+             this.stateName.push(data.state);
+
+             this.toatlCases.push(data.confirmed);
+             console.log(this.toatlCases);
+
+           });
       });
+      console.log('Outside',this.apiData);
+    // this.apiData.map((data) => {
+    //   //console.log(data);
+    //   this.stateName.push(data.state);
 
-    this.apiData.map((data) => {
-      //console.log(data);
-      this.stateName.push(data.state);
-
-      this.toatlCases = data.confirmed;
-    });
+    //   this.toatlCases = data.confirmed;
+    // });
 
     // console.log(this.stateName);
-    console.log(this.apiData);
+
   }
 
   updateValues(country) {
     console.log(country.target.value);
+
   }
 }
